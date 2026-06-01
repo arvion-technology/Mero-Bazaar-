e28d86d7d729647bb6c03139050070dc041e3e90
samjhanaG <kuinaa00@gmail.com>
2026-06-01 13:51:21 +0545

    feat: developed services and controllers

--- backend/src/modules/beauty/beauty.controller.ts
+++ backend/src/modules/beauty/beauty.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param, Patch, Get, Post, Body } from '@nestjs/common';
+import { HairBeautyAndWellnessService } from './beauty.service';
+import { CreateHairBeautyAndWellnessDto } from './dto/create_beauty.dto';
+import { UpdateHairBeautyAndWellnessDto } from './dto/update_beauty.dto';
 
 @Controller('beauty')
-export class BeautyController {}
+export class HairBeautyAndWellnessController {
+  constructor(private readonly beautyService: HairBeautyAndWellnessService) {}
+
+  @Post()
+  create(@Body() dto: CreateHairBeautyAndWellnessDto) {
+    return this.beautyService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.beautyService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.beautyService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateHairBeautyAndWellnessDto) {
+    return this.beautyService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.beautyService.remove(id);
+  }
+}
