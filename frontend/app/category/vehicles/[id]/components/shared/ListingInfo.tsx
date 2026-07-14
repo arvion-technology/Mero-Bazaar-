5bd9098eea88b24cbdf1dfee009d12256b7e96af
samjhanaG <kuinaa00@gmail.com>
2026-07-14 18:59:38 +0545

    fix: ci issue

--- frontend/app/category/vehicles/[id]/components/shared/ListingInfo.tsx
+++ frontend/app/category/vehicles/[id]/components/shared/ListingInfo.tsx
@@ -73,9 +73,7 @@ export default function ListingInfo({
             if (latitude == null || longitude == null) {
             window.open(
               `https://www.google.com/maps?q=${latitude},${longitude}`,"_blank");
-                } else if (location) {
-                  window.open(`https://www.google.com/maps/search/${encodeURIComponent(location)}`, "_blank");
-                }
+            } 
           }}
         >
           <FiMapPin size={11} /> View on map
