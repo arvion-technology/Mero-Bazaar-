d236561969b78123bd7b4a8bc020bd42f0d5138c
samjhanaG <kuinaa00@gmail.com>
2026-08-18 18:12:08 +0545

    fix: ci

--- frontend/app/user/orders/[id]/page.tsx
+++ frontend/app/user/orders/[id]/page.tsx
@@ -6,8 +6,7 @@ import { useParams, useRouter } from "next/navigation";
 import { useSession, signOut } from "next-auth/react";
 import { toast } from "react-toastify";
 import { MdVerified } from "react-icons/md";
-import { api } from "@/lib/api";
-import type { OrderDetail } from "@/app/types/orders";
+import type { OrderDetail as ApiOrderDetail } from "@/app/types/orders";
 import {
   FiGrid, FiShoppingBag, FiHeart, FiBell, FiHelpCircle, FiSettings,
   FiTrash2, FiAlertTriangle, FiLogOut, FiUser, FiChevronDown,
